6499bad46b2c4f186d06b90caf7ffb4557c44f98
Dylan Barkowsky <dylanbarkowsky@gmail.com>
2025-07-07 11:14:07 -0700

    maybe actually fix 404 this time

--- quartz/components/pages/404.tsx
+++ quartz/components/pages/404.tsx
@@ -3,14 +3,14 @@ import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } fro
 
 const NotFound: QuartzComponent = ({ cfg }: QuartzComponentProps) => {
   // If baseUrl contains a pathname after the domain, use this as the home link
-  const url = location.href; // will include the 404
-  const returnUrl = url.substring(0, url.lastIndexOf("/"));
+  const url = new URL(`https://${cfg.baseUrl ?? "example.com"}`)
+  const baseDir = url.pathname
 
   return (
     <article class="popover-hint">
       <h1>404</h1>
       <p>{i18n(cfg.locale).pages.error.notFound}</p>
-      <a href={returnUrl}>{i18n(cfg.locale).pages.error.home}</a>
+      <a href={baseDir}>{i18n(cfg.locale).pages.error.home}</a>
     </article>
   )
 }
